fdd5c201cb078f78c3837cdb4ec2c21776afb57d
Shohag Rana <shohagranasuvo@gmail.com>
2026-02-22 11:45:23 +0600

    script

--- lab 3/JavaScript/Script.js
+++ lab 3/JavaScript/Script.js
@@ -1,2 +1,21 @@
 console.log("connected!")
-alert("connected")
+//alert("connected")
+let a=90
+for(let i=0 ;i<10 ;i++)
+{
+    console.log(i+a)
+}
+var name ="AIUB"
+var name 
+var name ="AIUB"
+var name ="BUET"
+console.log(name)
+// let namee
+// namee=document.getElementById="Student_Name"
+// console.log(namee)
+ function collect_data(){
+   
+      let namee=document.getElementById("Student_Name").value
+      console.log(namee)
+
+     }
