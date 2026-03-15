1924533c356053cde06e1d3b17f39489a378460a
Shohag Rana <shohagranasuvo@gmail.com>
2026-03-15 11:30:36 +0600

    Lab Performance_MID folder update

---
+++ Lab Performance_MID/JS/Validation.js
@@ -0,0 +1,34 @@
+
+const unitPrice = 1000;
+const days = 30;
+let quantityInput = document.getElementById("quantity");
+let totalPriceInput = document.getElementById("totalPrice");
+
+
+function calculateTotal(){
+
+    let quantity = Number(quantityInput.value);
+
+   
+    if(quantity < 0)
+    {
+        alert("Quantity cannot be negative!");
+        quantity = 0;
+        quantityInput.value = 0;
+    }
+
+ 
+    let total = unitPrice * quantity * days;
+
+  
+   totalPriceInput.value=total;
+
+   
+    if(total > 1000)
+    {
+        alert(" Congratulations! You are eligible for a Gift Coupon!");
+    }
+
+};
+
+quantityInput.addEventListener("input", calculateTotal);
